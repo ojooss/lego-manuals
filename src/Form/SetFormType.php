cee1751f527c53fa0e19334c479069b0eb0f51c3
ojooss <53396131+ojooss@users.noreply.github.com>
2020-12-05 14:28:19 +0100

    numer should be NumberType

--- src/Form/SetFormType.php
+++ src/Form/SetFormType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\Set;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -15,7 +16,7 @@ class SetFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('number', TextType::class, ['label' => 'LEGO Set Nummer'])
+            ->add('number', NumberType::class, ['label' => 'LEGO Set Nummer'])
             ->add('name', TextType::class, ['label' => 'LEGO Set Name'])
         ;
 
