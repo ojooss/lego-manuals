edc97252812365a6f65d2bf689e190a58a6bce0d
ojooss <github@o-jooss.de>
2023-12-20 22:16:36 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
+# goto working dir
 cd /var/www/html/
 
+# update database schema
 php bin/console doctrine:migrations:migrate --no-interaction
 
+# start webserver
 apache2-foreground
