b8dd5fc94665f9695660621c438b43e098428f98
ojooss <github@o-jooss.de>
2026-05-30 12:24:26 +0200

    set file permissions dynamically based on APP_ENV in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,5 +6,14 @@ cd /var/www/html/
 # update database schema
 php bin/console doctrine:migrations:migrate --no-interaction
 
+    # set permissions
+if [ "$APP_ENV" = "dev" ]; then
+    chown -R 1000:www-data /var/www/html
+    chmod ug+w -R /var/www/html/var
+else
+    chown -R www-data:www-data /var/www/html
+    chmod ug+w -R /var/www/html/var
+fi
+
 # start webserver
 apache2-foreground
